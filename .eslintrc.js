bb4c431490f7cf09381c356b6a05e8d8867b2add
Kate Kostina <katekostina@yahoo.com>
2021-07-27 00:39:43 +0300

    Add render by layer

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'max-len': ["error", { "code": 105 }],
+    'max-len': ["error", { "code": 130 }],
     'no-unused-vars': "off",
     'guard-for-in': 'off',
     'no-restricted-syntax': 'off',
@@ -17,5 +17,7 @@ module.exports = {
     'no-plusplus': 'off',
     'import/prefer-default-export': 'off',
     'no-bitwise': 'off',
+    'no-param-reassign': 'off',
+    'no-return-assign': 'off',
   },
 };
